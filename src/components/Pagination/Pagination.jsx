aae22a9ce0f1ddd2a0ff9aeee3db98f05f9266b1
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-04-09 17:37:48 +0300

    add pagination style

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,7 +1,10 @@
 import { useDispatch } from 'react-redux';
+import sprite from '../../assets/sprite.svg';
+import useMediaQuery from '../../hooks/useMediaQuery.js';
 
 const Pagination = ({ page, totalPages, setPage }) => {
   const dispatch = useDispatch();
+  const isMobile = useMediaQuery('(max-width: 767px)');
 
   const handleNextPage = () => {
     dispatch(setPage(page + 1));
@@ -32,33 +35,76 @@ const Pagination = ({ page, totalPages, setPage }) => {
   const getVisiblePages = () => {
     const pages = [];
 
-    if (totalPages <= 4) {
-      for (let i = 1; i <= totalPages; i++) pages.push(i);
-    } else if (page >= totalPages - 2) {
-      pages.push('...', totalPages - 2, totalPages - 1, totalPages);
+    if (isMobile) {
+      if (totalPages <= 3) {
+        for (let i = 1; i <= totalPages; i++) pages.push(i);
+      } else if (page >= totalPages - 1) {
+        pages.push('...', totalPages - 1, totalPages);
+      } else {
+        pages.push(page, page + 1, '...');
+      }
     } else {
-      pages.push(page, page + 1, page + 2, '...');
+      if (totalPages <= 4) {
+        for (let i = 1; i <= totalPages; i++) pages.push(i);
+      } else if (page >= totalPages - 2) {
+        pages.push('...', totalPages - 2, totalPages - 1, totalPages);
+      } else {
+        pages.push(page, page + 1, page + 2, '...');
+      }
     }
 
     return pages;
   };
 
   return (
-    <div className=''>
-      <button className='' onClick={handleStartPage} disabled={page === 1}>
-        StartPrev
-      </button>
-      <button className='' onClick={handlePrevPage} disabled={page === 1}>
-        Prev
-      </button>
+    <div className='flex items-center justify-between md:justify-center md:gap-6'>
+      <div className='flex gap-1.5 md:gap-2'>
+        <button
+          className='group flex py-2.5 px-1.5 border border-inputs disabled:border-disabled rounded-full'
+          onClick={handleStartPage}
+          disabled={page === 1}
+        >
+          <svg
+            className='fill-text-dark group-disabled:opacity-[0.5] stroke-transparent -mr-1.5 md:w-6 md:h-6 md:-mr-2'
+            width={20}
+            height={20}
+          >
+            <use href={`${sprite}#icon-arrow-small`}></use>
+          </svg>
+          <svg
+            className='fill-text-dark group-disabled:opacity-[0.5] stroke-transparent -ml-1.5 md:w-6 md:h-6 md:-ml-2'
+            width={20}
+            height={20}
+          >
+            <use href={`${sprite}#icon-arrow-small`}></use>
+          </svg>
+        </button>
+        <button
+          className='group p-2.5 border disabled:border-disabled border-inputs rounded-full '
+          onClick={handlePrevPage}
+          disabled={page === 1}
+        >
+          <svg
+            className='fill-text-dark group-disabled:opacity-[0.5] stroke-transparent md:w-6 md:h-6'
+            width={20}
+            height={20}
+          >
+            <use href={`${sprite}#icon-arrow-small`}></use>
+          </svg>
+        </button>
+      </div>
 
-      <ul className=''>
+      <ul className='flex gap-2.5'>
         {getVisiblePages().map((pageNum, index) => (
           <li key={index}>
             <button
               key={pageNum}
               onClick={() => handlePageClick(pageNum)}
-              className={pageNum === page ? '' : ''}
+              className={
+                pageNum === page
+                  ? 'p-2.5 w-10 flex justify-center items-center rounded-full bg-brand text-text-white font-bold text-sm leading-[18px] md:w-11 md:h-11 md:text-lg md:leading-[22px]'
+                  : 'p-2.5 w-10 flex justify-center items-center border border-disabled rounded-full font-bold text-sm leading-[18px] md:w-11 md:h-11 md:text-lg md:leading-[22px]'
+              }
             >
               {pageNum}
             </button>
@@ -66,20 +112,41 @@ const Pagination = ({ page, totalPages, setPage }) => {
         ))}
       </ul>
 
-      <button
-        className=''
-        onClick={handleNextPage}
-        disabled={page === totalPages}
-      >
-        Next
-      </button>
-      <button
-        className=''
-        onClick={handleEndPage}
-        disabled={page === totalPages}
-      >
-        NextEnd
-      </button>
+      <div className='flex gap-1.5 md:gap-2'>
+        <button
+          className='group p-2.5 border border-inputs disabled:border-disabled rounded-full'
+          onClick={handleNextPage}
+          disabled={page === totalPages}
+        >
+          <svg
+            className='fill-text-dark group-disabled:opacity-[0.5] stroke-transparent rotate-180 md:w-6 md:h-6'
+            width={20}
+            height={20}
+          >
+            <use href={`${sprite}#icon-arrow-small`}></use>
+          </svg>
+        </button>
+        <button
+          className='group flex py-2.5 px-1.5 border disabled:border-disabled border-inputs rounded-full'
+          onClick={handleEndPage}
+          disabled={page === totalPages}
+        >
+          <svg
+            className='fill-text-dark group-disabled:opacity-[0.5] stroke-transparent rotate-180 -mr-1.5 md:w-6 md:h-6 md:-mr-2'
+            width={20}
+            height={20}
+          >
+            <use href={`${sprite}#icon-arrow-small`}></use>
+          </svg>
+          <svg
+            className='fill-text-dark group-disabled:opacity-[0.5] stroke-transparent rotate-180 -ml-1.5 md:w-6 md:h-6 md:-ml-2'
+            width={20}
+            height={20}
+          >
+            <use href={`${sprite}#icon-arrow-small`}></use>
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
