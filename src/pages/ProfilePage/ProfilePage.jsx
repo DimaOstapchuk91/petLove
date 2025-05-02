499b0ec9dd6530357922a55a119d7a83d5305cbf
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-05-02 20:17:38 +0300

    add component for profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,14 @@
+import MyNotices from '../../components/MyNotices/MyNotices.jsx';
+import UserCard from '../../components/UserCard/UserCard.jsx';
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <section className='container'>
+      <div className="pt-15 pb-20 md:pt-21.5 xl:pt-24'">
+        <UserCard />
+        <MyNotices />
+      </div>
+    </section>
+  );
 };
 export default ProfilePage;
