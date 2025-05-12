06f31567c0bba92dc3411d9df23b7d64aec8f9a7
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-05-13 00:31:23 +0300

    add not found text for news page

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -6,11 +6,21 @@ const NewsList = () => {
   const newsData = useSelector(selectDataNews);
 
   return (
-    <ul className='flex flex-col gap-6 mb-11 md:flex-row md:mb-15 md:flex-wrap md:gap-y-8 xl:gap-x-8.5 xl:gap-y-10'>
-      {newsData?.map(item => (
-        <NewsItem key={item._id} item={item} />
-      ))}
-    </ul>
+    <>
+      {!newsData.length > 0 ? (
+        <div className='px-8 py-5 md:py-15'>
+          <p className='text-center font-bold text-brand md:text-2xl xl:text-3xl'>
+            Sorry, nothing was found for this query.
+          </p>
+        </div>
+      ) : (
+        <ul className='flex flex-col gap-6 mb-11 md:flex-row md:mb-15 md:flex-wrap md:gap-y-8 xl:gap-x-8.5 xl:gap-y-10'>
+          {newsData?.map(item => (
+            <NewsItem key={item._id} item={item} />
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 export default NewsList;
